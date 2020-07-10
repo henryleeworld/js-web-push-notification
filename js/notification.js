a3995dee779b499b149cd4aa3eff4e37016bb9c3
Henry Lee <henryleeworld@gmail.com>
2020-07-11 00:03:01 +0800

    feat:初始化

---
+++ js/notification.js
@@ -0,0 +1,10 @@
+Push.create("〈特攝〉假面騎士BLACK", {
+    body: "這系列延續〝改造人〞的概念，主角南光太郎與好友秋月信彥被戈魯戈姆擄走並進行改造，南光太郎之後騎著戰鬥蝗蟲逃走，故事就圍繞著BLACK與戈魯戈姆的對抗，以及尋找秋月信彥的下落為主軸。",
+    icon: 'images/kamen-rider-black-icon.jpg',
+    timeout: 8000,
+    onClick: function() {
+        // window.focus();
+        // this.close();
+        window.location.replace("https://dic.pixiv.net/a/%E4%BB%AE%E9%9D%A2%E3%83%A9%E3%82%A4%E3%83%80%E3%83%BCBLACK");
+    }
+});
